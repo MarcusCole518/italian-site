28bd1d0ef3c23966eb872520ca1073a934c10990
“Marcus <mcolelittle@gmail.com>
2023-01-10 12:46:03 -0500

    fix typo

--- src/pages/it/example.js
+++ src/pages/it/example.js
@@ -2,7 +2,7 @@ import * as React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../../components/layout"
-import Seo from "../../seo/components/seo"
+import Seo from "../../components/seo"
 
 const ExamplePage = () => (
   <Layout>
